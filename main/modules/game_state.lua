9c393300cd7095980249e48a43fdb0cac4e04480
3D23 <dmitrichenkov.dima@mail.ru>
2024-11-21 13:51:14 +0300

    add statistic

--- main/modules/game_state.lua
+++ main/modules/game_state.lua
@@ -5,21 +5,23 @@ GAME_STATE = {}
 GAME_STATE.IS_PAUSED = false
 GAME_STATE.FINISHED = false
 GAME_STATE.STARTED = false
+GAME_STATE.LEVEL = 1
 
 GAME_STATE.EVENTS = {
-    on_game_pause = "on_game_pause",
-    on_game_resume = "on_game_resume",
-    on_finish_game = "on_finish_game",
-    on_start_game = "on_start_game"
+    on_level_pause = "on_level_pause",
+    on_level_resume = "on_level_resume",
+    on_finish_level = "on_finish_level",
+    on_start_level = "on_start_level",
+    on_change_level = "on_change_level"
 }
 
 ---@param value boolean
 GAME_STATE.set_is_paused = function (value)
     GAME_STATE.IS_PAUSED = value
     if GAME_STATE.IS_PAUSED == true then
-        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_game_pause)
+        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_level_pause)
     elseif GAME_STATE.IS_PAUSED == false then
-        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_game_resume)
+        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_level_resume)
     end
 end
 
@@ -27,7 +29,7 @@ end
 GAME_STATE.set_is_finished = function (value)
     GAME_STATE.FINISHED = value
     if GAME_STATE.FINISHED == true then
-        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_finish_game)
+        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_finish_level)
         GAME_STATE.STARTED = false
     end
 end
@@ -36,9 +38,14 @@ end
 GAME_STATE.set_is_started = function (value)
     GAME_STATE.STARTED = value
     if GAME_STATE.STARTED == true then
-        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_start_game)
+        event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_start_level)
         GAME_STATE.FINISHED = false
     end
+end
+
+GAME_STATE.set_level = function (value)
+    GAME_STATE.LEVEL = value
+    event_bus.EVENT_BUS:invoke(GAME_STATE.EVENTS.on_change_level, GAME_STATE.LEVEL)
 end
 
 return GAME_STATE
--- main/modules/game_statistic.lua
+++ main/modules/game_statistic.lua
@@ -1,6 +1,9 @@
 local event_bus = require('main.modules.event_bus_module')
 local GAME_STATE = require('main.modules.game_state')
 
+local ERROR_MESSAGE = "Could not add statistics to the statistics service."
+local SUCCESS_MESSAGE = "Success send statistic_data to statistic service"
+
 local GAME_STATISTIC = {}
 
 GAME_STATISTIC.EVENTS = {
@@ -12,19 +15,37 @@ GAME_STATISTIC.EVENTS = {
 GAME_STATISTIC.TOTAL_WAGONS = 0
 GAME_STATISTIC.DONE_WAGONS = 0
 GAME_STATISTIC.FAIL_WAGONS = 0
+GAME_STATISTIC.LEVEL = GAME_STATE.LEVEL
+
+local function create_statistic_request(statistic_data, headers, game_name)
+    local statistic_url = "https://ocrv-game.ru/statistic/api/v1/statistic"
+    local data = json.encode(statistic_data)
+    local body = json.encode({
+        Data = data,
+        GameName = game_name
+    })
+    http.request(statistic_url, 'POST', function(self, id, response)
+        if response.status == 201 then
+            print(SUCCESS_MESSAGE)
+        else
+            print(ERROR_MESSAGE)
+        end
+    end, headers, body)
+end
 
 function GAME_STATISTIC.init()
     event_bus.EVENT_BUS:subscribe(GAME_STATISTIC.EVENTS.on_end_way_wagon, GAME_STATISTIC.increase_total_wagons)
     event_bus.EVENT_BUS:subscribe(GAME_STATISTIC.EVENTS.on_done_wagon, GAME_STATISTIC.increase_done_wagons)
     event_bus.EVENT_BUS:subscribe(GAME_STATISTIC.EVENTS.on_fail_wagon, GAME_STATISTIC.increase_fail_wagons)
-    event_bus.EVENT_BUS:subscribe(GAME_STATE.EVENTS.on_finish_game, GAME_STATISTIC.clear_state)
+    event_bus.EVENT_BUS:subscribe(GAME_STATE.EVENTS.on_finish_level, GAME_STATISTIC.on_finish_level)
+    event_bus.EVENT_BUS:subscribe(GAME_STATE.EVENTS.on_change_level, GAME_STATISTIC.set_level)
 end
 
 function GAME_STATISTIC.dispose()
     event_bus.EVENT_BUS:unsubscribe(GAME_STATISTIC.EVENTS.on_end_way_wagon, GAME_STATISTIC.increase_total_wagons)
     event_bus.EVENT_BUS:unsubscribe(GAME_STATISTIC.EVENTS.on_done_wagon, GAME_STATISTIC.increase_done_wagons)
     event_bus.EVENT_BUS:unsubscribe(GAME_STATISTIC.EVENTS.on_fail_wagon, GAME_STATISTIC.increase_fail_wagons)
-    event_bus.EVENT_BUS:unsubscribe(GAME_STATE.EVENTS.on_finish_game, GAME_STATISTIC.clear_state)
+    event_bus.EVENT_BUS:unsubscribe(GAME_STATE.EVENTS.on_finish_level, GAME_STATISTIC.on_finish_level)
 end
 
 function GAME_STATISTIC.increase_total_wagons()
@@ -39,10 +60,45 @@ function GAME_STATISTIC.increase_fail_wagons()
     GAME_STATISTIC.FAIL_WAGONS = GAME_STATISTIC.FAIL_WAGONS + 1
 end
 
-function GAME_STATISTIC.clear_state()
+function GAME_STATISTIC.on_finish_level()
+    GAME_STATISTIC:send_statistic({
+        total_wagons = GAME_STATISTIC.TOTAL_WAGONS,
+        done_wagons = GAME_STATISTIC.DONE_WAGONS,
+        fail_wagons = GAME_STATISTIC.FAIL_WAGONS,
+        level = GAME_STATISTIC.LEVEL
+    })
     GAME_STATISTIC.TOTAL_WAGONS = 0
     GAME_STATISTIC.DONE_WAGONS = 0
     GAME_STATISTIC.FAIL_WAGONS = 0
+end
+
+---@param level number
+function GAME_STATISTIC.set_level(level)
+    GAME_STATISTIC.LEVEL = level
+end
+
+function GAME_STATISTIC:send_statistic(statistic_data)
+    local game_url = "https://ocrv-game.ru/statistic/api/v1/game"
+    local game_name = "wagons_way"
+    local headers = {["Content-Type"] = "application/json"}
+
+    http.request(game_url .. "/" .. game_name, 'GET', function(self, id, response) 
+        if response.status == 200 then
+            create_statistic_request(statistic_data, headers, game_name)
+        elseif response.status == 404 then
+            local game_body = json.encode({
+                Name = game_name,
+                Description = "_"
+            })
+            http.request(game_url, "POST", function (self, id, response)
+                if response.status == 201 then
+                    create_statistic_request(statistic_data, headers, game_name)
+                else
+                    print(ERROR_MESSAGE)
+                end
+            end, headers, game_body)
+        end
+    end, headers)
 end
 
 return GAME_STATISTIC
